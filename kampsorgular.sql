dc496c932e9a52ab9bc78f4d0388ef4fad87d38e
Burakhan Saruhan <burakhansaruhan271@gmail.com>
2024-10-23 00:41:17 +0300

    kullanılan postresql sorguları

---
+++ kampsorgular.sql
@@ -0,0 +1,51 @@
+-- CREATE TABLE TBLCategory (
+-- 	CategoryId int not null primary key,
+-- 	CategoryName VARCHAR(30) not null
+-- ) ;
+
+-- insert into tblcategory values (1, 'Ana Yemekler');
+-- insert into tblcategory values (2,'Tatlılar');
+-- insert into tblcategory values (3,'Salatalar');
+-- insert into tblcategory values (4,'Çorbalar');
+-- insert into tblcategory values (5,'Mezeler');
+-- insert into tblcategory values (6,'Pizzalar');
+-- insert into tblcategory values (7,'Hamburger');
+-- insert into tblcategory values (8,'Atıştırmalık');
+-- insert into tblcategory values (9,'Çocuk Menüleri');
+-- insert into tblcategory values (10,'Öğrenci Menüsü');
+
+-- CREATE TABLE TBLProduct (
+-- 	productid int not null primary key,
+-- 	productName VARCHAR(30) not null,
+-- 	productPrice decimal(18,2) not null
+	
+-- );
+-- insert into tblproduct values (1,'Mercimek Çorbası',60.00,false);
+-- insert into tblproduct values (2,'Yayla Çorbası',65.00,true);
+-- insert into tblproduct values (3,'Tavuk Suyu Çorbası',80.00,true);
+-- insert into tblproduct values (4,'Spagetti',50.00,true);
+-- insert into tblproduct values (5,'Steak Burger',100.00,true);
+-- insert into tblproduct values (6,'Cheese Burger',120.00,true);
+-- insert into tblproduct values (7,'Sütlaç',40.00,false);
+-- insert into tblproduct values (8,'Profiterol',35.00,true);
+-- insert into tblproduct values (9,'Köri Soslu Tavuk',90.00,true);
+-- insert into tblproduct values (10,'Kızarmış Mantı',85.00,true);
+-- update tblproduct set productStatus=true where productid=1;
+
+-- CREATE TABLE TBLOrder (
+-- 	orderId int not null primary key,
+-- 	productCount smallint,
+-- 	productUnitPrice decimal(18,2),
+-- 	totalPrice decimal(18,2)
+-- );
+
+
+
+
+
+
+
+
+
+
+
